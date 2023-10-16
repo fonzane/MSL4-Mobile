8d8d86b11bd1fe4d78ea712a4f3a76fb265b0fed
fonzane <timschaffert@yahoo.de>
2023-10-16 16:37:01 +0200

    trying to include webmonitor-login

--- MSL4-Mobile/MainPage.xaml.cs
+++ MSL4-Mobile/MainPage.xaml.cs
@@ -6,6 +6,8 @@ public partial class MainPage : ContentPage
 {
 
 	public string ip { get; set; }
+	public string email { get; set; }
+	public string password { get; set; }
 
 	public MainPage()
 	{
@@ -16,15 +18,39 @@ public partial class MainPage : ContentPage
     async void OnConnectToMSL(System.Object sender, System.EventArgs e)
     {
 		Console.WriteLine(ip);
-		await AuthService.GetSessionID(ip);
-		await Shell.Current.GoToAsync("//infoView");
-		//Navigation.PushAsync(new Views.Home.HomeView());
+		string resp = await AuthService.GetSessionID(ip);
+		Console.WriteLine(resp);
+		if (resp == "false")
+		{
+			await DisplayAlert("Verbindungsfehler", "Verbindung konnte nicht hergestellt werden.", "OK");
+		} else
+		{
+			Console.WriteLine("Verbindungsherstellung erfolgreich");
+			await Shell.Current.GoToAsync("//infoView");
+			//await Navigation.PushAsync(new Views.Home.HomeView());
+		}
 	}
 
-    //protected override void OnAppearing()
-    //{
-    //    base.OnAppearing();
-    //    Shell.SetTabBarIsVisible(Application.Current.MainPage, false);
-    //}
+	async void OnWebmonitorLogin(System.Object sender, System.EventArgs e)
+	{
+		bool loginResponse = await AuthService.LoginWebmonitor(email, password);
+		if (loginResponse)
+		{
+			string ip = await DisplayPromptAsync("Login Erfolgreich", "MSL4 IP-Adresse eingeben", "OK");
+			this.ip = ip;
+			OnConnectToMSL(null, null);
+    //        string resp = await AuthService.GetSessionID(ip);
+    //        if (resp == "false")
+    //        {
+    //            await DisplayAlert("Verbindungsfehler", "Verbindung konnte nicht hergestellt werden.", "OK");
+    //            return;
+    //        }
+    //        else
+    //        {
+				//Console.WriteLine("Connection successful" + resp);
+    //            await Shell.Current.GoToAsync("//infoView");
+    //        }
+        }
+	}
 
 }
--- MSL4-Mobile/Services/AuthService.cs
+++ MSL4-Mobile/Services/AuthService.cs
@@ -6,14 +6,18 @@ namespace MSL4_Mobile.Services;
 public class AuthService
 {
 	private static HttpClient client = new HttpClient();
-	public static string sessionid;
+    private static JsonSerializerOptions serializerOptions = new JsonSerializerOptions
+    {
+        Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+    };
+    public static string sessionid;
 	public static string ipaddress { get; set; } = "";
+	public static string auth_token { get; set; }
 	public static bool isConnected = false;
 
 	public static async Task<string> GetSessionID(string ip)
 	{
-		Console.WriteLine(ip);
-        client.DefaultRequestHeaders.Add("Cookie", "authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRzY2hhZmZlcnRAZnctc3lzdGVtZS5kZSIsIm5hbWUiOiJUaW0gU2NoYWZmZXJ0Iiwicm9sZXMiOlsic3VwZXJ1c2VyIl0sImRpdmlzaW9uIjoiVXNlciIsIl9pZCI6IjYxYjIwYTQ4ZWViZWY4YmEzYzkwMzE2OSIsImFkbWluaXN0cmF0b3MiOlsiNjFiMjBhNDhlZWJlZjhiYTNjOTAzMTY5Il0sImlhdCI6MTY3ODg4NzI1NiwiZXhwIjoxNjc5MjMyODU2fQ.qE7FwU7Vu34ZHJzCeYUBXCZiYEdslvhUca0LSSUxHCA");
+		client.DefaultRequestHeaders.Add("Cookie", "authorization=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRzY2hhZmZlcnRAZnctc3lzdGVtZS5kZSIsIm5hbWUiOiJUaW0gU2NoYWZmZXJ0Iiwicm9sZXMiOlsic3VwZXJ1c2VyIl0sImRpdmlzaW9uIjoiVXNlciIsIl9pZCI6IjYxYjIwYTQ4ZWViZWY4YmEzYzkwMzE2OSIsImFkbWluaXN0cmF0b3MiOlsiNjFiMjBhNDhlZWJlZjhiYTNjOTAzMTY5Il0sImlhdCI6MTY3ODg4NzI1NiwiZXhwIjoxNjc5MjMyODU2fQ.qE7FwU7Vu34ZHJzCeYUBXCZiYEdslvhUca0LSSUxHCA");
         ipaddress = ip;
 		Uri uri = new Uri($"http://{ip}/LogWeb/servlet/FOLogin?pUser=administrator&pPassword=admin");
 		AuthResponse authResponse = new AuthResponse();
@@ -32,8 +36,47 @@ public class AuthService
 		} catch (Exception ex)
 		{
 			Console.WriteLine(@"\tERROR {0}", ex.Message);
+			return "false";
 		}
 		return authResponse.sessionid.ToString();
+    }
+
+	public static async Task<bool> LoginWebmonitor(string email, string password)
+	{
+		client.Timeout = TimeSpan.FromSeconds(5);
+		Uri uri = new Uri("http://localhost:3000/auth/login");
+		LoginData loginData = new LoginData
+		{
+			email = email,
+			password = password
+		};
+        try
+        {
+            string json = JsonSerializer.Serialize<LoginData>(loginData, serializerOptions);
+            StringContent payload = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+            Console.WriteLine(json.ToString());
+
+			HttpResponseMessage response = await client.PostAsync(uri, payload);
+            if (response.IsSuccessStatusCode)
+            {
+                string stringResponse = await response.Content.ReadAsStringAsync();
+                LoginResponse loginResponse = JsonSerializer.Deserialize<LoginResponse>(stringResponse);
+				auth_token = loginResponse.access_token;
+                Console.WriteLine("Login successfull");
+				Console.WriteLine(auth_token);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("HTTP Request Failure: " + response.StatusCode);
+                return false;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(@"\tERROR {0}", ex.Message);
+            return false;
+        }
     }
 }
 
@@ -42,5 +85,16 @@ public class AuthResponse
 	public bool passwordok { get; set; }
 	public double sessionid { get; set; }
 	public bool userok { get; set; }
+}
+
+public record LoginResponse
+{
+	public string access_token { get; set; }
+}
+
+public class LoginData
+{
+	public string email { get; set; }
+	public string password { get; set; }
 }
 
